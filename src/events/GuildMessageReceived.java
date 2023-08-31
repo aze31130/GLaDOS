2fc07f6633f70a53660a80215404e82a723764a7
Guilhem <aze.adrarit@gmail.com>
2023-08-31 10:24:16 +0200

    Added fake dropbot message during downtime

--- src/events/GuildMessageReceived.java
+++ src/events/GuildMessageReceived.java
@@ -1,5 +1,6 @@
 package events;
 
+import java.awt.Color;
 import java.util.Arrays;
 import java.util.Random;
 import commands.Argument;
@@ -7,6 +8,7 @@ import commands.Command;
 import utils.Logger;
 import utils.Mention;
 import glados.GLaDOS;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -40,6 +42,21 @@ public class GuildMessageReceived extends ListenerAdapter {
 			// 	}
 			// 	a.totalExperience += 1;
 			// }
+
+			if (event.getMessage().getContentRaw().startsWith("|c")) {
+				EmbedBuilder embed = new EmbedBuilder();
+				embed.setTitle("Not claimed !");
+				embed.setDescription("Here are NOT your daily Schards: **+" + new Random().nextInt(3000) + "** || (??? in total)");
+				embed.setColor(Color.ORANGE);
+				event.getChannel().sendMessage(embed.build()).queue();
+			}
+
+			if (event.getMessage().getContentRaw().startsWith("|d")) {
+				EmbedBuilder embed = new EmbedBuilder();
+				embed.setTitle("You did not get a card today :-(");
+				embed.setColor(Color.gray);
+				event.getChannel().sendMessage(embed.build()).queue();
+			}
 			
 			if(event.getMessage().getContentRaw().contains(event.getJDA().getSelfUser().getId())) {
 				if(new Random().nextInt(100) >= 5) {
