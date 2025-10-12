5939b05be3c5097fd3ab2aee3ac72230e0a4c3e3
Guilhem <aze.adrarit@gmail.com>
2025-10-12 19:32:33 +0200

    Added market base api and removed useless space in ci

--- src/main/java/api/Market.java
+++ src/main/java/api/Market.java
@@ -1,11 +1,20 @@
 package api;
 
+import org.json.JSONArray;
+import glados.GLaDOS;
 import io.javalin.Javalin;
+import items.Item;
 
 public class Market {
 	public void register(Javalin api) {
 		api.get("/market", ctx -> {
-			ctx.result("TODO Market command");
+			GLaDOS g = GLaDOS.getInstance();
+			JSONArray result = new JSONArray();
+
+			for (Item i : g.market)
+				result.put(i.toJson());
+
+			ctx.json(result.toString());
 		});
 	}
 }
