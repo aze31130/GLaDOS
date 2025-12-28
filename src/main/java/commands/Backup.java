41625e6cc2c711be3115e968a948a50eb0a821c1
Guilhem <aze.adrarit@gmail.com>
2025-12-28 16:01:21 +0100

    Added embed field to backup command

--- src/main/java/commands/Backup.java
+++ src/main/java/commands/Backup.java
@@ -8,16 +8,17 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import accounts.Permission;
 import net.dv8tion.jda.api.entities.Message.Attachment;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.Command.Type;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.interactions.commands.Command.Type;
 import utils.FileUtils;
 
 public class Backup extends Command {
@@ -73,6 +74,16 @@ public class Backup extends Command {
 				for (Attachment attachment : message.getAttachments())
 					attachments.put(attachment.getUrl());
 				jsonMessage.put("attachments", attachments);
+
+				jsonMessage.put("embeds", message.getEmbeds().size());
+				JSONArray embeds = new JSONArray();
+				for (MessageEmbed embed : message.getEmbeds()) {
+					String title = embed.getTitle();
+
+					if (title != null)
+						embeds.put(title);
+				}
+				jsonMessage.put("embedsTitle", embeds);
 
 				JSONArray reactions = new JSONArray();
 				for (MessageReaction reaction : message.getReactions()) {
